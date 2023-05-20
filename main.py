591d4941ef944e61f14a5d867b481748f4c4efdb
Will <slorkda@gmail.com>
2023-05-20 14:22:59 -0700

    Changes in main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import math, random
 
 characters = [ ("Jack Boller", 50), ("Ali Baba", 35), ("Thief", 25) ]
+explosives = [ "black", "blue", "green", "red" ]
 
 # Convert a list of elements of the set {0, 1} into a floating point number
 def binary_list_to_float (x = None):
@@ -19,6 +20,16 @@ def binary_list_to_float (x = None):
 def doubly_open_ended_number ():
 	return inv_cdf(binary_list_to_float())
 
+def equipment ():
+	# Choose explosives / detonators
+	choice = random.choice(explosives)
+	print(characters[0][0] + ": " + choice)
+	print(characters[1][0] + ":", end = "")
+	for i in explosives:
+		if i != choice:
+			print(" " + i, end = "")
+	print()
+
 # Flip a coin x times
 def gen_binary_list (x = 10):
 	l = []
@@ -33,6 +44,7 @@ def gen_characters (x = 3):
 	for i in range(x):
 		s = characters[min(i, len(characters) - 1)]
 		print(s[0] + ": " + str(math.ceil(s[1] + doubly_open_ended_number())))
+	print("(Last " + characters[-1][0] + " gets +1 style point.)")
 
 # given a number between 0 and 1, return a number between -infinity and infinity
 def inv_cdf (x):
